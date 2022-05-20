7b8d4bfb211d650a60d9f73a3c65c16a462be090
doanh11 <trancongdoanh150896@gmail.com>
2022-05-20 09:04:42 +0700

    fix

--- baitap4.js
+++ baitap4.js
@@ -1,13 +1,13 @@
 function taoTheDiv(){
     // var result = document.getElementById ("result").style.display="block" ;
     var content ="" ;
-    for (var i = 0;i < 10 ; i++){
-        var leDiv = `<div div style= "background-color:blue">lẻ</div>`;
-        var chanDiv =  `<div div style= "background-color:red">Chẵn</div>`;
-        if (i % 2 !== 0){
-            content += leDiv ;
-        }else {
+    for (var i = 1 ;i <= 10 ; i++){
+        var leDiv = `<div div style= "background-color:blue">Div lẻ ${i}</div>`;
+        var chanDiv =  `<div div style= "background-color:red">Div Chẵn ${i}</div>`;
+        if (i % 2 === 0){
             content += chanDiv ;
+        }else {
+            content += leDiv ;
         }
     }
     document.getElementById("result").innerHTML = content ;
